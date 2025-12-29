d6e4e1e49b59daf17eadcbd9a654f329eb525298
GojurJeevan <jeevangojur2605@gmail.com>
2025-12-29 23:16:49 +0530

    Updated

--- src/categories/Categories.jsx
+++ src/categories/Categories.jsx
@@ -1,7 +1,26 @@
 export const Categories = () => {
   return (
     <>
-      <div className="text-white text-5xl font-light text-center mt-25 opacity-80">Top Products</div>
+      <div className="text-white text-5xl font-light text-center mt-25 opacity-80">
+        Top Products
+      </div>
+      <div className="flex justify-between ml-40 mr-40 mt-20">
+        <div>
+          <button className="text-white text-xl font-light opacity-60 hover:bg-red-500 h-10 w-max rounded-xl">All</button>
+        </div>
+        <div>
+          <button className="text-white text-xl font-light opacity-60 hover:bg-red-500 h-10 w-max rounded-xl">HeadPhones</button>
+        </div>
+        <div>
+          <button className="text-white text-xl font-light opacity-60 hover:bg-red-500 h-10 w-max rounded-xl">Earbuds</button>
+        </div>
+        <div>
+          <button className="text-white text-xl font-light opacity-60 hover:bg-red-500 h-10 w-max rounded-xl">Earphones</button>
+        </div>
+        <div>
+          <button className="text-white text-xl font-light opacity-60 hover:bg-red-500 h-10 w-max rounded-xl">Neckbands</button>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/categories/CategorySlice.jsx
@@ -0,0 +1,6 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let CategorySlice = createSlice({
+    name:'category',
+    initialState:0,
+})
