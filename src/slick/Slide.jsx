29f859bfca37cfa9e567ecc9ee37b11953dfda7f
GojurJeevan <jeevangojur2605@gmail.com>
2025-12-29 19:18:54 +0530

    Update

--- src/slick/Slide.jsx
+++ src/slick/Slide.jsx
@@ -2,24 +2,29 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import banner1 from "../assets/products/banner1.png"
+import banner1 from "../assets/products/banner1.png";
 
 export default function Slide() {
   var settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
   };
+
   return (
-    <Slider {...settings}>
+    <>
+      <Slider {...settings}>
         <div>
-            <img src={banner1} alt="Banner 1" className="h-200" />
+          <div className="flex justify-between">
+            <p className="text-5xl">Hello</p>
+            <div>
+              <img src={banner1} alt="Banner 1" className="h-100" />
+            </div>
+          </div>
         </div>
-        <div>
-            <img src={banner1} alt="Banner 1" className="h-200" />
-        </div>
-    </Slider>
+      </Slider>
+    </>
   );
 }
